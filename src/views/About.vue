494ba91697926fa4b8014586980725f14db9cdcb
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-09-15 17:59:47 -0300

    Added vue router and views

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It´s part of
+      the Vue crash course on Youtube chanel TraversyMedia.
+    </p>
+    <span>Channel: </span><a href="https://youtu.be/Wy9q22isx3U">Traversy Media</a>
+  </div>
+</template>
+
+<style scoped>
+  .about a {
+    font-weight: 800;
+    color: #2c3e50;
+    text-decoration: none;
+  }
+</style>
